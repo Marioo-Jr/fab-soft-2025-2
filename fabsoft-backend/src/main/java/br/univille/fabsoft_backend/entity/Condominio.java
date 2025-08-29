970493561f18cd3bd6fb2383ea3a3d52594f1063
Mario Sergio <141345989+Marioo-Jr@users.noreply.github.com>
2025-08-29 00:57:22 +0000

    tabelas arrumadas

--- fabsoft-backend/src/main/java/br/univille/fabsoft_backend/entity/Condominio.java
+++ fabsoft-backend/src/main/java/br/univille/fabsoft_backend/entity/Condominio.java
@@ -12,7 +12,6 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "condominio_tb")
 public class Condominio {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
